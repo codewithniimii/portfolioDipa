748cb4aa0376d91a445111a8bbc65a2f7cf08fd6
Dipa Gaire <dgaire45@gmail.com>
2025-02-06 16:56:56 +0200

    hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";  
 import "./globals.css";
-
+import StarsCanvas from "@/components/main/StarBackground";
+import Navbar from "@/components/main/Navbar";
 
 const roboto = Roboto({
   weight: ['400', '700'], 
@@ -22,6 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${roboto.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
+        <StarsCanvas/>
+        <Navbar/>
         {children}
       </body>
     </html>
